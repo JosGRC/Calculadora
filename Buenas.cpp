7e1b575f97116d479b982f3a2cc79d92e577c7c9
JosGRC <98417128+JosGRC@users.noreply.github.com>
2022-05-17 12:44:00 -0500

    Add files via upload

---
+++ Buenas.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int time = 0;
+	if (1,2,3,4,5,6,7,8,9,10,11==time){
+		cout << "Good morning."<<endl;
+	} 
+	if(12,13,14==time){
+		cout << "its noon."<<endl;
+	}
+	if(15,16,17,18==time){
+		cout << "Good evening"<<endl;
+	}
+	if(19,20,21,22,23==time){
+		cout << "Good night"<<endl;
+	}
+	if(0==time){
+		cout << "Good midnight"<<endl;
+	}
+}
